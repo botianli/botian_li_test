fac6533702025579cd7ca5a3dc627f51da14c046
botianli <43157192+botianli@users.noreply.github.com>
2020-01-29 15:09:16 -0500

    Create Q2_tester.py

---
+++ Q2_tester.py
@@ -0,0 +1,18 @@
+from StringLib import CompareTwoString
+
+
+def main():
+  a='10'
+  b='10'
+  CompareTwoString(a,b)
+  a="1.1"
+  b="0"
+  CompareTwoString(a, b)
+  a="a"
+  b="b"
+  CompareTwoString(a, b)
+
+
+
+if __name__=='__main__':
+    main()
